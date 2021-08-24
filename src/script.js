c7b4852b02e468a92f1d1d3e056a284571c839f5
jyywong <wong.jonathan1@gmail.com>
2021-08-24 15:54:57 -0400

    Finished blender model

--- src/script.js
+++ src/script.js
@@ -1,27 +1,126 @@
 import './style.css';
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import * as TWEEN from '@tweenjs/tween.js';
 
 const canvas = document.querySelector('.webgl');
 
+const sizes = {
+	width: window.innerWidth,
+	height: window.innerHeight
+};
+
 const scene = new THREE.Scene();
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const mesh = new THREE.Mesh(geometry, material);
+const axesHelper = new THREE.AxesHelper(3);
+scene.add(axesHelper);
 
-scene.add(mesh);
+// Loaders
+const textureLoader = new THREE.TextureLoader();
 
-const sizes = {
-	width: 800,
-	height: 600
-};
+const gltfLoader = new GLTFLoader();
+
+// Textures
+const bakedTexture = textureLoader.load('blender/baked2.jpg');
+bakedTexture.flipY = false;
+
+// Materials
+const bakedMaterial = new THREE.MeshBasicMaterial({ map: bakedTexture });
+
+// Camera
+
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.z = 3;
+
 scene.add(camera);
+
+// Model
+
+gltfLoader.load('blender/portfolioOptimized2.glb', (gltf) => {
+	gltf.scene.traverse((child) => {
+		// console.log(child);
+		child.material = bakedMaterial;
+	});
+	const hMonitor = gltf.scene.children.find((child) => child.name === 'hMonitor');
+	const shelf = gltf.scene.children.find((child) => child.name === 'shelf');
+	// Position to look at hMonitor
+	// x: -1.5427103804282107
+	// y: 1.2007012737959566
+	// z: -0.28940390033229746
+
+	camera.position.x = -1.5427103804282107;
+	camera.position.y = 1.2007012737959566;
+	camera.position.z = -0.28940390033229746;
+	camera.lookAt(shelf.position);
+
+	console.log(shelf);
+
+	// camera.lookAt(shelfBooks.position);
+	// control.target = hMonitor.position;
+
+	scene.add(gltf.scene);
+});
 
 const renderer = new THREE.WebGLRenderer({
 	canvas
 });
 renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-renderer.render(scene, camera);
+// Handle resize
+window.addEventListener('resize', () => {
+	sizes.width = window.innerWidth;
+	sizes.height = window.innerHeight;
+
+	// Update camera
+	camera.aspect = sizes.width / sizes.height;
+	camera.updateProjectionMatrix();
+
+	// Update renderer
+	renderer.setSize(sizes.width, sizes.height);
+	renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+// // Handle user input
+// window.addEventListener('click', () => {
+// 	console.log('hello');
+// 	const { x, y, z } = camera.position;
+// 	const { x: rX, y: rY, z: rZ } = camera.rotation;
+// 	const coords = { x, y, z, rX, rY, rZ };
+// 	const tween = new TWEEN.Tween(coords)
+// 		.to(
+// 			{
+// 				x: 3,
+// 				y: 3,
+// 				z: 3,
+// 				rX: -0.7853981571068328,
+// 				rY: 0.6154797114558709,
+// 				rZ: 0.5235987718562193
+// 			},
+// 			2000
+// 		)
+// 		.onUpdate(() => {
+// 			camera.position.set(coords.x, coords.y, coords.z);
+// 			camera.rotation.set(coords.rX, coords.rY, coords.rZ);
+// 			// camera.updateProjectionMatrix();
+// 		})
+// 		.onStop(() => {
+// 			camera.lookAt(new THREE.Vector3());
+// 		});
+// 	tween.start();
+// });
+
+const control = new OrbitControls(camera, renderer.domElement);
+control.enableDamping = true;
+
+const clock = new THREE.Clock();
+const tick = () => {
+	const elapsedTime = clock.getElapsedTime();
+	// console.log(camera.rotation);
+	TWEEN.update();
+	control.update();
+	renderer.render(scene, camera);
+	window.requestAnimationFrame(tick);
+};
+
+tick();
